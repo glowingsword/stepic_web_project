a19c8eeb22d6d6030fda4576ce282fc41573db15
Andrei Gutu <glowingsword@gmail.com>
2016-03-09 16:01:29 +0200

    ask application was added

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-rm /etc/nginx/sites-enabled/default 
-﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+rm -f /etc/nginx/sites-enabled/default 
+ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
+rm -f /etc/gunicorn.d/test
+
 ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+rm -f /etc/gunicorn.d/ask
+ln -sf /home/box/web/etc/gunicorn_ask.conf   /etc/gunicorn.d/ask
 /etc/init.d/gunicorn restart
