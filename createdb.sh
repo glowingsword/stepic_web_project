cd6a34f5ba60c6b0348b686d4dc2b9f34ddc9add
Andrei Gutu <glowingsword@gmail.com>
2016-03-09 21:53:10 +0200

    Fixed error in sh-scripts

--- createdb.sh
+++ createdb.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-mysql -uroot -e 'create database ask;'
-mysql -uroot -e 'CREATE USER 'ask'@'localhost' IDENTIFIED BY 'Jocho4Za';'
+mysql -uroot -e "create database ask;"
 mysql -uroot -e "CREATE USER 'ask'@'localhost' IDENTIFIED BY 'Jocho4Za';"
-mysql -uroot -e "GRANT ALL ON ask.* TO 'ask'@'local;"
+mysql -uroot -e "GRANT ALL ON ask.* TO 'ask'@'localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
