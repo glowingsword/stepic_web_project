25141660cfae2bd659054e67730ef7b77c1d3bc3
Andrei Gutu <glowingsword@gmail.com>
2016-03-09 16:49:13 +0200

    ask app added in nginx.conf

--- init.sh
+++ init.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
+
+easy_install -m
+rm -rf /usr/lib/python2.7/dist-packages/Django-1.6.1.egg-info
+rm -rf /usr/lib/python2.7/dist-packages/django
+
+pip install --upgrade Django
+echo 'Python version'
+python -c "import django; print(django.get_version())"
+echo 'Python path'
+python -c "import django; print(django.__path__)"
+
 rm -f /etc/nginx/sites-enabled/default 
 ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
