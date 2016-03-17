c604519a7e4da15f4b70fb545ca90bde6b16ff39
Andrei Gutu <glowingsword@gmail.com>
2016-03-17 02:25:34 +0200

    fixed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
   text = models.TextField(default='')
   added_at = models.DateTimeField(default=timezone.now)
   rating = models.IntegerField(default=0)
-  author = models.ForeignKey(User, null=True)
+  author = models.ForeignKey(User, default=1)
   likes = models.ManyToManyField(User, related_name="question_likes", blank=True)
   class Meta:
     ordering = ('added_at',)
@@ -23,7 +23,7 @@ class Answer(models.Model):
   text = models.TextField()
   added_at = models.DateTimeField(default=timezone.now)
   question = models.ForeignKey(Question,null=True)
-  author = models.ForeignKey(User, null=True)
+  author = models.ForeignKey(User, default=1)
   class Meta:
     ordering = ('added_at',)
   def __unicode__(self):
--- init.sh
+++ init.sh
@@ -4,18 +4,19 @@
 #apt-get install python-mysqldb
 
 #pip install --upgrade Django
-#echo 'Django version'
-#python -c "import django; print(django.get_version())"
+echo 'Django version'
+python -c "import django; print(django.get_version())"
 #echo 'Django installation path'
 #python -c "import django; print(django.__path__)"
 
 rm -f /etc/nginx/sites-enabled/default 
-ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-/etc/init.d/nginx restart
-rm -f /etc/gunicorn.d/test
 
+rm -f /etc/gunicorn.d/test
 ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 
 rm -f /etc/gunicorn.d/ask
 ln -sf /home/box/web/etc/gunicorn_ask.conf   /etc/gunicorn.d/ask
 /etc/init.d/gunicorn restart
+
+ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
